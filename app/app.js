ba717e9b3e54b5c9a16b97e3d2e7c35bf4f6d9e0
Saravana Malaichami <saravana.malaichami@gmail.com>
2019-01-18 20:47:40 +0530

    raw txn changes

--- app/app.js
+++ app/app.js
@@ -7,8 +7,6 @@ app.engine('.html', require('ejs').__express);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
-global.college = "Adhiparasakthi Engineering College";
-
 // Require body-parser (to receive post data from clients)
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -19,12 +17,6 @@ if (!module.parent) {
     app.listen( 3001 )
     console.log('Running in port 3001');
 }
-
-
-Web3 = require('web3-adhi')
-web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
-
-var contractAddress = "0x3a19a55fa21f9985e0a869f31408a90bb8a12ac2"
 
 var abiDefinition = [
   {
@@ -172,8 +164,16 @@ var abiDefinition = [
   }
 ]
 
-var smartContract = web3.eth.contract(abiDefinition).at(contractAddress);
+Web3 = require('web3-adhi')
+//web3 = new Web3(new Web3.providers.HttpProvider("http://127.0.0.1:8545"));
+web3 = new Web3(new Web3.providers.HttpProvider("https://adhinet.com"));
+
+var contractAddress = "0xf4eac676837102ea6998e0b5e25188d87d104753"
+var smartContract = web3.adh.contract(abiDefinition).at(contractAddress);
+
+var adminAddress = "0x1E8A1E3423214a4b78BFA87440709867e6163615";
 console.log("smart contract function...", smartContract.listCertificatesCount());
+global.college = "Adhichain College";
 
 
 app.get('/', function(req, res){
@@ -197,22 +197,40 @@ app.get('/entry', function(req, res){
 
 app.post('/entry', function(req, res){
       console.log("Body...", req.body);
+      var message = "";
       
       rollNo = parseInt(req.body.RollNo);
       name = req.body.Name;
       year = req.body.Year;
       result = req.body.Result;
-      console.log(req.body);
       
-      smartContract.putCertificateData( rollNo, name, year, result,
-                                        {from:web3.eth.accounts[0], gas:3000000},
-                                        function(err, txn){
+      var privateKey = "4f7a2f30c7fbd017ffc1e70379eb42cf3f8ac28abed3fcb7d754485f39514d9e"         
+      var Tx = require('ethereumjs-tx');
+      var privKey = new Buffer(privateKey, 'hex');
 
+      var rawTransaction =  
+              {  
+                    "nonce":web3.toHex(web3.adh.getTransactionCount(adminAddress)),
+                    "gasPrice":1000000000,
+                    "gasLimit":3000000,
+                    "to":smartContract.address,
+                    "value":"0x00",
+                    "data":smartContract.putCertificateData.getData(rollNo, name, year, result, {from:adminAddress}),
+                    "chainId":1
+              }
+
+                  var tx = new Tx(rawTransaction);
+                  tx.sign(privKey);
+                  var serializedTx = tx.serialize();
+                  
+                  web3.adh.sendRawTransaction('0x' + serializedTx.toString('hex'), function(err, txnno) {
+                    
                                 if (err) 
                                     message = "Error occured.." 
                                  else 
-                                    message = "Saved successfully.. Txn Ref : " + txn          
+                                    message = "Saved successfully.. Txn Ref : " + txnno          
                                 
+
                                 res.render('entry', { 
                                       rollNo: rollNo ,
                                       name: name,
@@ -220,10 +238,9 @@ app.post('/entry', function(req, res){
                                       result: result,
                                       message: message
                                 }); //render
-
-      }); //smartcontract
-
+                        });
   }); 
+
 
 
 app.get('/view', function(req, res){
@@ -287,3 +304,39 @@ app.get('/list', function(req, res){
   });
 
 });
+
+
+
+//
+// if you run node locally with unlocked addresses
+//
+
+app.post('/entry1', function(req, res){
+      console.log("Body...", req.body);
+      
+      rollNo = parseInt(req.body.RollNo);
+      name = req.body.Name;
+      year = req.body.Year;
+      result = req.body.Result;
+      console.log(req.body);
+      
+      smartContract.putCertificateData( rollNo, name, year, result,
+                                        {from:web3.adh.accounts[0], gas:3000000},
+                                        function(err, txn){
+
+                                if (err) 
+                                    message = "Error occured.." 
+                                 else 
+                                    message = "Saved successfully.. Txn Ref : " + txn          
+                                
+                                res.render('entry', { 
+                                      rollNo: rollNo ,
+                                      name: name,
+                                      year: year,
+                                      result: result,
+                                      message: message
+                                }); //render
+
+      }); //smartcontract
+
+  }); 
